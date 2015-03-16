53eaed79a5dd863ab0c17f582281648802713cc4
zerob13 <zerob13@gmail.com>
2015-03-16 11:32:35 +0800

    format code

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,12 +4,12 @@ $(function() {
     if (state === 0) {
       $('.md-button').addClass('left-arrow');
       state = 1;
-    } else if(state===1){
+    } else if (state === 1) {
       state = 2;
       $('.md-button').removeClass('left-arrow');
       $('.md-button').addClass('hook');
-    }else{
-      state=0; 
+    } else {
+      state = 0;
       $('.md-button').removeClass('hook');
     }
   });
